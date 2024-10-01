599caf34cb83a6807380116df0eb08340a3da4ce
hitchhooker <tommi@niemi.lol>
2024-10-02 00:15:36 +0700

    add totalFlareSharesState

--- src/mappings/sflr.ts
+++ src/mappings/sflr.ts
@@ -1,21 +1,24 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { AccrueRewards } from "../../generated/SFLRContract/SFLR"
-import { ExchangeRate, AccrueReward } from "../../generated/schema"
+import { ExchangeRate, AccrueReward, StakingState } from "../../generated/schema"
 import { SFLR } from "../../generated/SFLRContract/SFLR"
 
-// Handle Accrue Rewards and update exchange rate
 export function handleAccrueRewards(event: AccrueRewards): void {
   let contract = SFLR.bind(event.address)
   let oneShare = BigInt.fromI32(10).pow(18) // 1 SFLR with 18 decimals
+  let totalShares = contract.totalShares()
   let flrAmount = contract.getPooledFlrByShares(oneShare)
 
-  // Update ExchangeRate entity (use AccrueRewards event to update exchange rate)
+  let stakingState = new StakingState(event.transaction.hash.toHex())
+  stakingState.totalShares = totalShares
+  stakingState.timestamp = event.block.timestamp
+  stakingState.save()
+
   let exchangeRate = new ExchangeRate(event.transaction.hash.toHex())
   exchangeRate.rate = flrAmount
   exchangeRate.timestamp = event.block.timestamp
   exchangeRate.save()
 
-  // Handle AccrueReward entity
   let accrueReward = new AccrueReward(event.transaction.hash.toHex())
   accrueReward.timestamp = event.block.timestamp
   accrueReward.userRewardAmount = event.params.userRewardAmount
